42061ceee309526bbf80de422a60af87e4c18a7f
dongdev.hn <dongdev.hn@gmail.com>
2026-09-14 18:30:14 +0700

    fix: revert vercel rewrites and use nip.io urls for traefik

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   keycloak: {
-    url: '/auth',
+    url: 'https://auth.194.233.71.64.nip.io',
     realm: 'bpmn-realm',
     clientId: 'bpmn-dashboard'
   },
-  bffApiUrl: '/api',
+  bffApiUrl: 'https://api.194.233.71.64.nip.io/api',
   postgrestFallbackUrl: 'http://194.233.71.64:3000'
 };
